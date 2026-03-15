75ba66039f1a56d4c1dc20deac34b5f6ada32419
Diego121985 <diegoleite59986@gmail.com>
2026-03-14 22:30:45 -0300

    test4

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
@@ -7,7 +7,7 @@ import { CartProvider } from "./contexts/CartContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
- <BrowserRouter basename="/burger-express">
+  <BrowserRouter>
     <CartProvider>
       <App />
     </CartProvider>
